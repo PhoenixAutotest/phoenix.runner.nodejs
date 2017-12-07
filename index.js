3cee8a042780d95420e596b8a08d9e27562712b1
surenpi <zxjlwt@126.com>
2017-12-07 16:50:24 +0800

    完成了基本功能的测试

--- index.js
+++ index.js
@@ -1,38 +1,87 @@
 #!/usr/bin/env node
 
+var fs = require('fs');
+var os = require('os');
+var cli = require('cli');
+var program = require('commander');
 var exec = require('child_process').exec;
 
-var fs = require('fs');
-function copy(src, dst) {
-    fs.writeFileSync(dst, fs.readFileSync(src));
-}
+program.version('0.0.1')
+    .option('-u, --update', '更新程序')
+    .option('-r, --runners <items>', '测试套件')
+    .option('-c, --cheese [type]', 'Add the specified type of cheese [marble]', 'marble')
+    .parse(process.argv);
 
-var os = require('os');
-console.log(os.homedir());
-var baseDir = os.homedir() + '/phoenix';
-console.log(baseDir);
+var OPTIONS = {
 
-function exe() {
-    copy(__dirname + '/pom.xml', baseDir + '/pom.xml');
-    copy(__dirname + '/test.xml', baseDir + '/test.xml');
-    copy(__dirname + '/page-demo.xml', baseDir + '/page-demo.xml');
-    //mvn.execute(['-f ' + baseDir + '/pom.xml', 'dependency:copy-dependencies']);
-    exec('mvn -f ' + baseDir + '/pom.xml package', function(){
-        var exec = require('child_process').exec;
-        exec('java -jar ' + baseDir + '/autotest.suite.runner-1.0.1-20170824-20171205.093339-1.jar -runners test.xml', function (err, stdout, stderr) {
+};
+
+var exports = {
+    interpret: function (args) {
+        cli.setArgv(args);
+        cli.setApp(path.resolve(__dirname + "/package.json"));
+    },
+    copy: function (src, dst) {
+        fs.writeFileSync(dst, fs.readFileSync(src));
+    },
+    copyDemo: function () {
+        var baseDir = os.homedir() + '/phoenix';
+
+        this.copy(__dirname + '/pom.xml', baseDir + '/pom.xml');
+        this.copy(__dirname + '/test.xml', baseDir + '/test.xml');
+        this.copy(__dirname + '/page-demo.xml', baseDir + '/page-demo.xml');
+    },
+    getBaseDir: function () {
+        var baseDir = os.homedir() + '/phoenix';
+
+        return baseDir;
+    },
+    update: function (callback) {
+        var baseDir = os.homedir() + '/phoenix';
+
+        function exe() {
+            exec('mvn -f ' + baseDir + '/pom.xml package', function(){
+                if((typeof callback) === 'function') {
+                    callback();
+                }
+            });
+        }
+
+        fs.exists(baseDir, function (exists) {
+            if(!exists) {
+                fs.mkdir(baseDir, function(){
+                    exe();
+                });
+            }else{
+                exe();
+            }
+        });
+    },
+    execute: function () {
+        var baseDir = this.getBaseDir();
+        var param = this.getPhoenixParam();
+        var cmd = 'java -jar ' + baseDir + '/autotest.suite.runner-1.0.1-20170824-20171205.093339-1.jar' + param;
+        console.log(cmd);
+
+        exec(cmd, function (err, stdout, stderr) {
             console.log(stdout);
             console.log(err);
         });
-    });
-}
+    },
+    getPhoenixParam: function () {
+        var param = '';
+        if(program.runners) {
+            param += ' -runners ' + program.runners;
+        }
 
-fs.exists(baseDir, function (exists) {
-    console.log(exists);
-    if(!exists) {
-        fs.mkdir(baseDir, function(){
-            exe();
-        });
-    }else{
-        exe();
+        return param;
     }
-});
+};
+
+module.exports = exports;
+
+if(program.update) {
+    exports.update(exports.execute);
+} else {
+    exports.execute();
+}
